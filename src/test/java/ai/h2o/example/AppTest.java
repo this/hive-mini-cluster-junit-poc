7a6fec5ee39535532336468db141260d5bb13ea0
Sajith Ariyarathna <sajith.janaprasad@gmail.com>
2021-04-17 01:44:23 +0530

    Use junit temp directory instead of hard-coded paths

--- src/test/java/ai/h2o/example/AppTest.java
+++ src/test/java/ai/h2o/example/AppTest.java
@@ -7,9 +7,13 @@ import com.github.sakserv.minicluster.impl.HiveLocalMetaStore;
 import com.github.sakserv.minicluster.impl.HiveLocalServer2;
 import com.github.sakserv.minicluster.impl.ZookeeperLocalCluster;
 import org.apache.hadoop.hive.conf.HiveConf;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.nio.file.Path;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,41 +22,48 @@ public class AppTest {
     private static HiveLocalMetaStore hiveLocalMetaStore;
     private static HiveLocalServer2 hiveLocalServer2;
 
-    @BeforeClass
-    public static void setUp() throws Exception {
+    @Rule
+    public TemporaryFolder tmpDir = new TemporaryFolder();
+
+    @Before
+    public void setUp() throws Exception {
+        final Path zookeeperDir = tmpDir.newFolder("zk-mini-cluster").toPath();
         zookeeperLocalCluster = new ZookeeperLocalCluster.Builder()
                 .setPort(22010)
-                .setTempDir("/tmp/zk-mini-cluster")
+                .setTempDir(zookeeperDir.toString())
                 .setZookeeperConnectionString("127.0.0.1:22010")
                 .build();
         zookeeperLocalCluster.start();
 
+        final Path hiveDir = tmpDir.newFolder("hive-mini-cluster").toPath();
+        final String hiveMetastoreDerbyDbDir = hiveDir.resolve("metastore_db").toString();
+        final String hiveScratchDir = hiveDir.resolve("hdfs-scratchdir").toString();
+        final String hiveWarehouseDir = hiveDir.resolve("warehouse").toString();
         hiveLocalMetaStore = new HiveLocalMetaStore.Builder()
                 .setHiveMetastoreHostname("localhost")
                 .setHiveMetastorePort(9083)
-                .setHiveMetastoreDerbyDbDir("/tmp/hive-mini-cluster/metastore_db")
-                .setHiveScratchDir("/tmp/hive-mini-cluster/hdfs-scratchdir")
-                .setHiveWarehouseDir("/tmp/hive-mini-cluster/warehouse")
+                .setHiveMetastoreDerbyDbDir(hiveMetastoreDerbyDbDir)
+                .setHiveScratchDir(hiveScratchDir)
+                .setHiveWarehouseDir(hiveWarehouseDir)
                 .setHiveConf(new HiveConf())
                 .build();
         hiveLocalMetaStore.start();
-
         hiveLocalServer2 = new HiveLocalServer2.Builder()
                 .setHiveServer2Hostname("localhost")
                 .setHiveServer2Port(10000)
                 .setHiveMetastoreHostname("localhost")
                 .setHiveMetastorePort(9083)
-                .setHiveMetastoreDerbyDbDir("/tmp/hive-mini-cluster/metastore_db")
-                .setHiveScratchDir("/tmp/hive-mini-cluster/hdfs-scratchdir")
-                .setHiveWarehouseDir("/tmp/hive-mini-cluster/warehouse")
+                .setHiveMetastoreDerbyDbDir(hiveMetastoreDerbyDbDir)
+                .setHiveScratchDir(hiveScratchDir)
+                .setHiveWarehouseDir(hiveWarehouseDir)
                 .setZookeeperConnectionString("127.0.0.1:22010")
                 .setHiveConf(new HiveConf())
                 .build();
         hiveLocalServer2.start();
     }
 
-    @AfterClass
-    public static void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         hiveLocalServer2.stop();
         hiveLocalMetaStore.stop();
         zookeeperLocalCluster.stop();
